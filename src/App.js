dc8213f1183cd15000fb497baab8a20fe831077f
bonarja <bonarja@gmail.com>
2020-09-06 01:28:20 +0200

    magicScrollHero

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useEffect, useRef } from "react";
 
 import { Controller, Scene } from "react-scrollmagic";
 import { Tween, Timeline } from "react-gsap";
-import "./App.css";
+import "./App.scss";
 import Hero from "./sections/hero";
 import Loader from "./componets/loader";
 import Intro from "./componets/introVideo";
@@ -24,13 +24,124 @@ const App = () => {
         // }, 1000);
     }, []);
 
+    // return (
+    //     <div className="section cover" style={{ overflow: "scroll" }}>
+    //         <Controller>
+    //             <Scene
+    //                 duration={"30%"}
+    //                 pin={{ pushFollowers: false }}
+    //                 triggerHook={0}
+    //             >
+    //                 <div className="sticky cover center">
+    //                     <h1 style={{ color: "white" }}>Pin Test</h1>
+    //                 </div>
+    //             </Scene>
+    //             <div className="Content">
+    //                 <Scene
+    //                     duration={200}
+    //                     pin={{ pushFollowers: false }}
+    //                     triggerHook={0.9}
+    //                 >
+    //                     <div className="sticky">
+    //                         <div>Pin Test</div>
+    //                     </div>
+    //                 </Scene>
+    //             </div>
+    //             {/* <Scene duration={300} pin={true} offset={100}>
+    //                 <div className="sticky blue">
+    //                     <div>Pin Test</div>
+    //                 </div>
+    //             </Scene> */}
+    //         </Controller>
+    //     </div>
+    // );
+
+    return (
+        <div className="bonarja cover finishIntro">
+            {ShowIntroVideo.get ? <Intro onFinish={onFinishVid}></Intro> : ""}
+            <Hero ref={$hero}></Hero>
+            <div className="Content"></div>
+            {/* <div id="trigger" className="cover">
+                <div className="coverHero cover"></div>
+                <div className="Content"></div>
+               <Controller>
+                    <Scene
+                        triggerElement="#trigger"
+                        duration={500}
+                        pin={false}
+                        indicators={true}
+                    >
+                        {(progress) => (
+                            <Tween
+                                from={{
+                                    css: {
+                                        marginTop: 0,
+                                    },
+                                    // ease: "Strong.easeOut",
+                                }}
+                                to={{
+                                    css: {
+                                        marginTop: -300,
+                                    },
+                                    // ease: "Strong.easeOut",
+                                }}
+                                totalProgress={progress}
+                                paused
+                            >
+                                <div className="Content"> hola mundo</div>
+                            </Tween>
+                        )}
+                    </Scene>
+                </Controller> 
+            </div> */}
+        </div>
+    );
     return (
         <div className="bonarja cover finishIntro">
             {/* <Loader></Loader> */}
 
             {ShowIntroVideo.get ? <Intro onFinish={onFinishVid}></Intro> : ""}
-
-            <Hero ref={$hero}></Hero>
+            <Controller>
+                <div className={`HERO cover`} style={{ height: "100vh" }}>
+                    <Scene
+                        duration={"30%"}
+                        pin={{ pushFollowers: true }}
+                        // indicators={true}
+                        // triggerHook={0}
+                        // enabled={true}
+                        // triggerElement={".HERO"}
+                    >
+                        {/* <Hero ref={$hero}></Hero> */}
+                        <div className="sticky cover center">
+                            <div style={{ color: "white" }}>Pin Test</div>
+                        </div>
+                        {/* <div className={`HERO cover`}>
+                        <Hero ref={$hero}></Hero>
+                        <h1>hola </h1>
+                    </div> */}
+                    </Scene>
+                </div>
+                {/* <div className={`Content`}> */}
+                {/* <div> */}
+                {/* <Scene
+                    duration={400}
+                    pin={true}
+                    // offset={100}
+                    // reverse={true}
+                >
+                    <div className={`Content cover`}></div>
+                </Scene> */}
+                {/* </div> */}
+                {/* <Scene duration={300} pin={true} offset={100}>
+                    <h1>hola mundo</h1>
+                </Scene> */}
+                {/* </div> */}
+                {/* <div className={`Content`}>
+                    <Scene triggerElement="#trigger" pin={true}>
+                        <h2 id="#trigger">hola mundo</h2>
+                    </Scene>
+                </div> */}
+            </Controller>
             {/* <div id="trigger">
                 <Controller>
                     <Scene
--- src/sections/hero.js
+++ src/sections/hero.js
@@ -5,6 +5,8 @@ import LineWord from "../componets/lineword";
 import HeroVid from "../assets/hero.mp4";
 import Sfx from "../assets/sfx.mp3";
 import StateController from "../modules/stateController";
+import { Controller, Scene } from "react-scrollmagic";
+import { Tween } from "react-gsap";
 
 const Hero = forwardRef((props, ref) => {
     const $line1 = useRef();
@@ -12,12 +14,20 @@ const Hero = forwardRef((props, ref) => {
     const $line3 = useRef();
     const $video = useRef();
     const $audio = useRef();
+    const $letter = useRef();
 
     const ActiveIntro = StateController("");
 
     const playLineFx = () => {
         $audio.current.currentTime = 0;
         $audio.current.play();
+    };
+
+    const moveLetter = (progress) => {
+        if (!$letter.current) return;
+        // console.log($letter.current);
+        $letter.current.style.marginTop = `-${300 * progress}px`;
+        $letter.current.style.marginLeft = `-${300 * progress}px`;
     };
 
     useImperativeHandle(ref, () => ({
@@ -29,56 +39,101 @@ const Hero = forwardRef((props, ref) => {
     }));
     return (
         <div id="hero" className={`${css.Hero} cover center y`}>
-            <div className={`${css.wallpaper} cover`}>
-                <div className={`${css.layer} cover`}></div>
-                <video
-                    playsInline
-                    loop={true}
-                    ref={$video}
-                    className={ActiveIntro.get}
+            <Controller>
+                <Scene
+                    triggerElement="#hero"
+                    duration={"90%"}
+                    pin={false}
+                    // indicators={true}
+                    offset={"100%"}
+                    triggerHook={0}
                 >
-                    <source src={HeroVid}></source>
-                </video>
-            </div>
-
-            <div className={`${css.content} cover`}>
-                <div className={`${css.letter} cover center y`}>
-                    <audio ref={$audio}>
-                        <source src={Sfx}></source>
-                    </audio>
-                    <LineWord
-                        text="Hi"
-                        ref={$line1}
-                        width="15"
-                        finishInterval={0.2}
-                        onFinish={() => $line2.current.active()}
-                        textColor="white"
-                        left="10"
-                        lineColor="white"
-                        onPlay={playLineFx}
-                    ></LineWord>
-                    <LineWord
-                        text="i am"
-                        ref={$line2}
-                        width="30"
-                        finishInterval={0.2}
-                        onFinish={() => $line3.current.active()}
-                        textColor="white"
-                        left="8"
-                        lineColor="#ff9c07"
-                        textColor="#ff9c07"
-                    ></LineWord>
-                    <LineWord
-                        text="kleyber"
-                        ref={$line3}
-                        width="55"
-                        textColor="#d4826f"
-                        lineColor="#d4826f"
-                        size="12"
-                        left="10"
-                    ></LineWord>
+                    {(progress) => (
+                        <Tween
+                            from={{
+                                css: {
+                                    top: 0,
+                                },
+                                ease: "Strong.easeOut",
+                            }}
+                            to={{
+                                css: {
+                                    top: "3%",
+                                },
+                                ease: "Strong.easeOut",
+                            }}
+                            totalProgress={progress}
+                            paused
+                        >
+                            <div className={`${css.wallpaper} cover wallpaper`}>
+                                <div className={`${css.layer} cover`}></div>
+                                <video
+                                    playsInline
+                                    loop={true}
+                                    ref={$video}
+                                    className={ActiveIntro.get}
+                                >
+                                    <source src={HeroVid}></source>
+                                </video>
+                            </div>
+                        </Tween>
+                    )}
+                </Scene>
+                <Scene
+                    triggerElement="#hero"
+                    duration={"40%"}
+                    pin={false}
+                    // indicators={true}
+                    offset={"90%"}
+                    triggerHook={0}
+                >
+                    {(progress) => {
+                        moveLetter(progress);
+                        return <></>;
+                    }}
+                </Scene>
+                <div id="HeroContent" className={`${css.content} cover`}>
+                    <div
+                        className={`${css.letter} cover center y`}
+                        ref={$letter}
+                    >
+                        <audio ref={$audio}>
+                            <source src={Sfx}></source>
+                        </audio>
+                        <LineWord
+                            text="Hi"
+                            ref={$line1}
+                            width="15"
+                            finishInterval={0.2}
+                            onFinish={() => $line2.current.active()}
+                            textColor="white"
+                            left="10"
+                            lineColor="white"
+                            onPlay={playLineFx}
+                        ></LineWord>
+                        <LineWord
+                            text="i am"
+                            ref={$line2}
+                            width="30"
+                            finishInterval={0.2}
+                            onFinish={() => $line3.current.active()}
+                            textColor="white"
+                            left="8"
+                            lineColor="#ff9c07"
+                            textColor="#ff9c07"
+                        ></LineWord>
+                        <LineWord
+                            text="kleyber"
+                            ref={$line3}
+                            width="55"
+                            textColor="#d4826f"
+                            lineColor="#d4826f"
+                            size="12"
+                            left="10"
+                        ></LineWord>
+                    </div>
                 </div>
-            </div>
+            </Controller>
         </div>
     );
 });
